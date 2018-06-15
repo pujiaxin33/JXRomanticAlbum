d0b5c2d4541b255aa38e8db081333b2b6c1f641e
pjx <pujx@jingbo360.com>
2018-06-15 16:58:41 +0800

    引入lottie

---
+++ Podfile
@@ -0,0 +1,11 @@
+
+platform :ios, '9.0'
+use_frameworks!
+inhibit_all_warnings!
+
+
+target ‘JXRomanticAlbum’ do
+
+pod 'lottie-ios', '2.1.5'
+
+end
---
+++ Pods/Target Support Files/Pods-JXRomanticAlbum/Pods-JXRomanticAlbum-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_JXRomanticAlbum : NSObject
+@end
+@implementation PodsDummy_Pods_JXRomanticAlbum
+@end
